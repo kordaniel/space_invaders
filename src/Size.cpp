2b4d54f9da21f9e7082819dde67d0753c1d19869
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2021-04-08 21:06:00 +0300

    Add Size-class

---
+++ src/Size.cpp
@@ -0,0 +1,8 @@
+#include "Size.h"
+
+Size::Size(size_t width, size_t height):
+    width(width),
+    height(height)
+{
+    //
+}
---
+++ src/Size.h
@@ -0,0 +1,14 @@
+#ifndef SIZE_H
+#define SIZE_H
+
+#include <cstdlib>
+
+class Size
+{
+    public:
+        size_t width;
+        size_t height;
+        Size(size_t, size_t);
+};
+
+#endif
