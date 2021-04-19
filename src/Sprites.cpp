e77f958591cfcbe23524ac50a9d6b37fc3dc8988
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2021-04-19 16:06:03 +0300

    Refactor sprites data to be immutable

--- src/Sprites.cpp
+++ src/Sprites.cpp
@@ -7,6 +7,17 @@ Sprite::Sprite(int32_t width, int32_t height, const uint8_t* sprite_data):
     //
 }
 
+const uint8_t* Sprite::getSpritePtr(int32_t offset)
+{
+    const uint8_t* copy = data + offset;
+    return copy;
+}
+
+const uint8_t* Sprite::getNumberSpritePtr(int32_t number)
+{
+    return getSpritePtr(16 * getTotalSize() + number * getTotalSize());
+}
+
 Sprites::Sprites(void):
     player_sprite(Sprite(11, 7, _PLAYER_SPRITE)),
     player_bullet_sprite(Sprite(1, 3, _PLAYER_BULLET_SPRITE)),
--- src/Sprites.h
+++ src/Sprites.h
@@ -12,12 +12,14 @@ class Sprite: public Size
     public:
         const uint8_t* data;
         Sprite(int32_t, int32_t, const uint8_t*);
+        const uint8_t* getSpritePtr(int32_t offset);
+        const uint8_t* getNumberSpritePtr(int32_t number);
 };
 
 class Sprites
 {
     private:
-        const uint8_t* _PLAYER_SPRITE = (uint8_t[]) {
+        const uint8_t* const _PLAYER_SPRITE = (uint8_t[]) {
             // size: 11 * 7 = 77
             0,0,0,0,0,1,0,0,0,0,0, // .....@.....
             0,0,0,0,1,1,1,0,0,0,0, // ....@@@....
@@ -27,8 +29,8 @@ class Sprites
             1,1,1,1,1,1,1,1,1,1,1, // @@@@@@@@@@@
             1,1,1,1,1,1,1,1,1,1,1  // @@@@@@@@@@@
         };
-        const uint8_t* _PLAYER_BULLET_SPRITE = (uint8_t[]) { 1,1,1 };
-        const uint8_t* _ALIEN_DEATH_SPRITE = (uint8_t[]) {
+        const uint8_t* const _PLAYER_BULLET_SPRITE = (uint8_t[]) { 1,1,1 };
+        const uint8_t* const _ALIEN_DEATH_SPRITE = (uint8_t[]) {
             // size: 13 * 7 = 91
             0,1,0,0,1,0,0,0,1,0,0,1,0, // .@..@...@..@.
             0,0,1,0,0,1,0,1,0,0,1,0,0, // ..@..@.@..@..
@@ -38,7 +40,7 @@ class Sprites
             0,0,1,0,0,1,0,1,0,0,1,0,0, // ..@..@.@..@..
             0,1,0,0,1,0,0,0,1,0,0,1,0  // .@..@...@..@.
         };
-        const uint8_t* _ALIEN_SPRITE_0 = (uint8_t[]) {
+        const uint8_t* const _ALIEN_SPRITE_0 = (uint8_t[]) {
             // size: 8 * 8 = 64
             0,0,0,1,1,0,0,0, // ...@@...
             0,0,1,1,1,1,0,0, // ..@@@@..
@@ -49,7 +51,7 @@ class Sprites
             1,0,0,0,0,0,0,1, // @......@
             0,1,0,0,0,0,1,0  // .@....@.
         };
-        const uint8_t* _ALIEN_SPRITE_1 = (uint8_t[]) {
+        const uint8_t* const _ALIEN_SPRITE_1 = (uint8_t[]) {
             // size: 8 * 8 = 64
             0,0,0,1,1,0,0,0, // ...@@...
             0,0,1,1,1,1,0,0, // ..@@@@..
@@ -60,7 +62,7 @@ class Sprites
             0,1,0,1,1,0,1,0, // .@.@@.@.
             1,0,1,0,0,1,0,1  // @.@..@.@
         };
-        const uint8_t* _ALIEN_SPRITE_2 = (uint8_t[]) {
+        const uint8_t* const _ALIEN_SPRITE_2 = (uint8_t[]) {
             // size 11 * 8 = 88
             0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
             0,0,0,1,0,0,0,1,0,0,0, // ...@...@...
@@ -71,7 +73,7 @@ class Sprites
             1,0,1,0,0,0,0,0,1,0,1, // @.@.....@.@
             0,0,0,1,1,0,1,1,0,0,0  // ...@@.@@...
         };
-        const uint8_t* _ALIEN_SPRITE_3 = (uint8_t[]) {
+        const uint8_t* const _ALIEN_SPRITE_3 = (uint8_t[]) {
             // size 11 * 8 = 88
             0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
             1,0,0,1,0,0,0,1,0,0,1, // @..@...@..@
@@ -82,7 +84,7 @@ class Sprites
             0,0,1,0,0,0,0,0,1,0,0, // ..@.....@..
             0,1,0,0,0,0,0,0,0,1,0  // .@.......@.
         };
-        const uint8_t* _ALIEN_SPRITE_4 = (uint8_t[]) {
+        const uint8_t* const _ALIEN_SPRITE_4 = (uint8_t[]) {
             // size 12 * 8 = 96
             0,0,0,0,1,1,1,1,0,0,0,0, // ....@@@@....
             0,1,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@@.
@@ -93,7 +95,7 @@ class Sprites
             0,0,1,1,0,1,1,0,1,1,0,0, // ..@@.@@.@@..
             1,1,0,0,0,0,0,0,0,0,1,1  // @@........@@
         };
-        const uint8_t* _ALIEN_SPRITE_5 = (uint8_t[]) {
+        const uint8_t* const _ALIEN_SPRITE_5 = (uint8_t[]) {
             // size 12 * 8 = 96
             0,0,0,0,1,1,1,1,0,0,0,0, // ....@@@@....
             0,1,1,1,1,1,1,1,1,1,1,0, // .@@@@@@@@@@.
@@ -104,11 +106,11 @@ class Sprites
             0,1,1,0,0,1,1,0,0,1,1,0, // .@@..@@..@@.
             0,0,1,1,0,0,0,0,1,1,0,0  // ..@@....@@..
         };
-        const uint8_t* _ALIEN_BULLET_SPRITE_0 = // length: 3 * 7 = 21
+        const uint8_t* const _ALIEN_BULLET_SPRITE_0 = // length: 3 * 7 = 21
             (uint8_t[]) {0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0};
-        const uint8_t* _ALIEN_BULLET_SPRITE_1 = // length: 3 * 7 = 21
+        const uint8_t* const _ALIEN_BULLET_SPRITE_1 = // length: 3 * 7 = 21
             (uint8_t[]) {0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0};
-        const uint8_t* _TEXT_SPRITESHEET = (uint8_t[]) {
+        const uint8_t* const _TEXT_SPRITESHEET = (uint8_t[]) {
             // length 5 * 7 * 65 = 2275
             // ASCII characters in the range [32,96] == ['space','`']
             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
