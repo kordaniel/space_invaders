ee05a08f2bd973e6c0517ede584702c83b46bc6a
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2021-04-23 13:37:47 +0300

    Include <cassert>

--- src/global.h
+++ src/global.h
@@ -1,12 +1,20 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-/*
+/* *************************************************
  * Define DEBUG to compile the project with included
- * error printing.
+ * error printing and assertions. Define NDEBUG to
+ * disable the same for production builds.
+ * NOTE: Define only one of these. Also note that
+ * your compiler might (un)define these!!!
  */
+
 #define DEBUG
+//#define NDEBUG
+
+/* ********************************************** */
 
 #include <cstdint>
+#include <cassert>
 
 #endif
