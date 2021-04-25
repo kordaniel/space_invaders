101a1171d46fff6cfa7b6382aba53fe19e184165
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2021-04-25 16:17:26 +0300

    Add classes for timing the lifetime of a scope

---
+++ src/Timer.cpp
@@ -0,0 +1,43 @@
+#include "Timer.h"
+
+using namespace std::chrono;
+
+Timer::Timer(bool printTimingResults)
+    : m_printTimingRes(printTimingResults)
+{
+    m_startTimepoint = steady_clock::now();
+}
+
+Timer::~Timer(void)
+{
+    if (m_printTimingRes) {
+        time_point<steady_clock> endTimepoint = steady_clock::now();
+        processResult(endTimepoint, nullptr);
+    }
+}
+
+void Timer::processResult(const time_point<steady_clock>& endTimepoint, const char* message)
+{
+    int64_t startTime =
+        time_point_cast<microseconds>(m_startTimepoint).time_since_epoch().count();
+    int64_t endTime =
+        time_point_cast<microseconds>(endTimepoint).time_since_epoch().count();
+    
+    int64_t duration = endTime - startTime;
+    double duration_ms = duration * 0.001;
+    io::print_to_stdout_varargs((message != nullptr ? message : ""), duration, "us (", duration_ms, "ms)");
+}
+
+
+ScopeTimer::ScopeTimer(const char* scopeName)
+    : m_scopeName(scopeName)
+    , m_Timer(false)
+{
+    //
+}
+
+ScopeTimer::~ScopeTimer(void)
+{
+    time_point<steady_clock> endTimepoint = steady_clock::now();
+    m_Timer.processResult(endTimepoint, m_scopeName);
+}
---
+++ src/Timer.h
@@ -0,0 +1,32 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "global.h"
+#include "Io.h"
+
+#include <chrono>
+
+class Timer
+{
+public:
+    Timer(bool printTimingResults = true);
+    ~Timer(void);
+    void processResult(const std::chrono::time_point<std::chrono::steady_clock>& endTimepoint, const char* message);
+
+private:
+    bool m_printTimingRes;
+    std::chrono::time_point<std::chrono::steady_clock> m_startTimepoint;    
+};
+
+
+class ScopeTimer
+{
+public:
+    ScopeTimer(const char* scopeName);
+    ~ScopeTimer(void);
+private:
+    const char* m_scopeName;
+    Timer m_Timer; // NOTE: This must always be initialized as the last member for maximum accuracy!!
+};
+
+#endif
