d684109a522b6a6f38018d2bae4c234a94891550
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2021-05-02 19:54:51 +0300

    Add macrowrappers to aid debugging

--- src/global.h
+++ src/global.h
@@ -1,7 +1,9 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-/* *************************************************
+/* **************************************************
+ * TODO: !!! Refactor all code to use only NDEBUG !!!
+ *
  * Define DEBUG to compile the project with included
  * error printing and assertions. Define NDEBUG to
  * disable the same for production builds.
@@ -11,8 +13,7 @@
 
 #define DEBUG
 //#define NDEBUG
-
-/* ********************************************** */
+/* *********************************************** */
 
 #ifdef DEBUG
 #include "Timer.h"
@@ -20,15 +21,37 @@
 
 #include <cstdint>
 #include <cstddef>
+
 #include <cassert>
 
+#ifndef NDEBUG
+// NOTE: We havent included "Io.h" here, so if the code wont compile this must be handled.
+//       This is because then we would have different includes for DEBUG/RELEASE modes.
+    #define assertpair(exp, arg1, arg2)\
+        do {\
+            if (!(exp))\
+                io::print_to_stderr_varargs("ASSERTION failed: (", arg1, " <=> ", #exp, " <=> ", arg2, "):");\
+            else (void)0;\
+            assert(exp);\
+        } while (false)
+
+    #define NEWoper(variable, command) \
+        do {\
+            io::print_to_stdout_varargs(__FILE__, ", ", __LINE__, ": Allocating heap mem.");\
+            variable = command;\
+        } while (false)
+#else
+    #define assertpair(exp, arg1, arg2) (void)0
+    #define NEWoper(variable, command) variable = command
+#endif
+
 enum SpaceobjectType: size_t {
-    ALIEN_A    = 0,
-    ALIEN_B    = 2,
-    ALIEN_C    = 4,
-    ALIEN_DEAD = 5,
-    PLAYER     = 6,
-    BULLET_ALIEN = 7,
+    ALIEN_A       = 0,
+    ALIEN_B       = 2,
+    ALIEN_C       = 4,
+    ALIEN_DEAD    = 5,
+    PLAYER        = 6,
+    BULLET_ALIEN  = 7,
     BULLET_PLAYER = 8
 };
 
