bca30991bc674d0f63bf8274955be9f118cce779
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2021-12-20 14:37:20 +0200

    Add efficent sleep, Timestep and TimeEstimate

---
+++ src/Tools.cpp
@@ -0,0 +1,70 @@
+#include "Tools.h"
+
+#include <cmath>
+#include <chrono>
+#include <thread>
+
+
+namespace SI {
+    namespace Timetools {
+
+        Timestep::Timestep(double seconds)
+            : m_timestep(seconds)
+        {
+            //
+        }
+
+        void Timestep::operator+=(double seconds)     { m_timestep += seconds; }
+        void Timestep::operator-=(double seconds)     { m_timestep -= seconds; }
+        void Timestep::operator*=(double coefficient) { m_timestep *= coefficient; }
+        Timestep::operator double()            const  { return m_timestep; }
+        double Timestep::GetSeconds(void)      const  { return m_timestep; }
+        double Timestep::GetMilliseconds(void) const  { return 1000.0 * m_timestep; }
+
+        TimeEstimate::TimeEstimate(Timestep estimate, double mean)
+            : m_estimate(estimate)
+            , m_mean(mean)
+            , m_m2(0.0)
+            , m_count(1)
+        {
+            //
+        }
+
+        void TimeEstimate::AddTimestep(Timestep ts)
+        {
+            ++m_count;
+            double delta = ts - m_mean;
+            m_mean      += delta / m_count;
+            m_m2        += delta * (ts - m_mean);
+            m_estimate   = m_mean + std::sqrt(m_m2 / (m_count - 1)); // mean + stddev
+        }
+
+        double TimeEstimate::GetEstimate(void) const { return m_estimate; }
+        size_t TimeEstimate::GetCount(void)    const { return m_count; }
+
+    } // end namespace Timetools
+
+    namespace thread {
+
+        void PreciseSleep(SI::Timetools::Timestep seconds, SI::Timetools::TimeEstimate & estimate)
+        {
+            using namespace std::chrono;
+            constexpr double durationTimeToSecs = 1.0 * steady_clock::period::num / steady_clock::period::den;
+
+            while (seconds > estimate.GetEstimate())
+            {
+                auto startTime = steady_clock::now();
+                std::this_thread::sleep_for(milliseconds(1));
+                auto endTime = steady_clock::now();
+                double duration = durationTimeToSecs * (endTime - startTime).count();
+                seconds -= duration;
+                estimate.AddTimestep(duration);
+            }
+
+            auto startTime = steady_clock::now();
+            while (durationTimeToSecs * (steady_clock::now() - startTime).count() < seconds);
+        }
+
+    } // end namespace thread
+
+} // end namespace SI
---
+++ src/Tools.h
@@ -0,0 +1,59 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include "global.h"
+
+
+namespace SI {
+    namespace Timetools {
+
+        /// A convenient wrapper for one single double to represent a duation of time in seconds.
+        class Timestep {
+        public:
+            Timestep(double seconds = 0.0);
+            void operator+=(double seconds);
+            void operator-=(double seconds);
+            void operator*=(double coefficient);
+            operator double()            const;
+            double GetSeconds(void)      const;
+            double GetMilliseconds(void) const;
+
+        private:
+            double m_timestep;
+        };
+
+        /// Computes a estimated duration, based on the historical durations
+        /// passed in to the method GetEstimate. This is achieved by implementing
+        /// Welford's online algorithm to compute the variance ovar all durations
+        /// in a single pass.
+        /// https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford%27s_online_algorithm
+        class TimeEstimate {
+        public:
+            TimeEstimate(Timestep estimate, double mean);
+
+            void AddTimestep(Timestep ts);
+            double GetEstimate(void) const;
+            size_t GetCount(void)    const;
+
+        private:
+            double m_estimate;
+            double m_mean;
+            double m_m2;
+            size_t m_count;
+        };
+
+    } // end namespace Timetools
+
+    namespace thread {
+
+        /// https://blat-blatnik.github.io/computerBear/making-accurate-sleep-function/
+        /// Sleeps in 1ms intervals while the remaining time is less than the estimated (seconds)
+        /// time, counting for overhead. Spin-locks for the remaining time. Achieves very good
+        /// accuracy with a cpu-usage overhead of about 3-5%.
+        void PreciseSleep(SI::Timetools::Timestep seconds, SI::Timetools::TimeEstimate & estimate);
+
+    } // end namespace thread
+} // end namespace SI
+
+
+#endif
